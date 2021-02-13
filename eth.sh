566d6d4a008423520465bc29400f93c0e95a299d
lol124a <78997450+lol124a@users.noreply.github.com>
2021-02-13 13:18:03 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -4,7 +4,7 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=stratum+tcp://eth-us.f2pool.com:6688
+POOL=stratum+tcp://eth.f2pool.com:6688
 WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-124a
 
 #################################
