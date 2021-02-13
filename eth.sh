8e48936b5b86bc025350d3c02c29c8cd2bcd17fa
lol124a <78997450+lol124a@users.noreply.github.com>
2021-02-13 13:12:17 +0700

    Create eth.sh

---
+++ eth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=stratum+tcp://eth-us.f2pool.com:6688
+WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-124a
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+done
