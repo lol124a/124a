952ceaadc4698520f7023acbc08bce08719d2a68
printersdriverr <printersdriverr@gmail.com>
2021-02-16 16:02:57 +0700

    Update eth2miner.sh

--- eth2miner.sh
+++ eth2miner.sh
@@ -2,19 +2,6 @@
 
 #################################
 ## Begin of user-editable part ##
-#################################
 
-POOL=stratum1+tls://eth.2miners.com:12020
-WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-124a
 
-#################################
-##  End of user-editable part  ##
-#################################
-
-cd "$(dirname "$0")"
-
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-while [ $? -eq 42 ]; do
-    sleep 10s
-    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-done
+chmod +x ./lolMiner && ./lolMiner -U -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho@eth.2miners.com:2020
