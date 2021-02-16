a80931855f834a611df55af8133bd19ef7a345dd
printersdriverr <printersdriverr@gmail.com>
2021-02-16 16:08:01 +0700

    Update eth2miner.sh

--- eth2miner.sh
+++ eth2miner.sh
@@ -4,4 +4,4 @@
 ## Begin of user-editable part ##
 
 
-chmod +x ./lolMiner && ./lolMiner -P stratum1+tcp://0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho@eth.2miners.com:2020
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x70278496f0eaa3810d9dbdd7f388425e029013db.Leumangtho
